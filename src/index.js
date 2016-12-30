0ef6d161ce123e77d7d321cabf781ce6b05dacfd
Nikunj Sharma <nikunj.sharma@jugnoo.in>
2016-12-30 14:59:21 +0530

    youtube search webapp

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = "AIzaSyC4CLhXijY2LjvOnJSxMz0qE3J1Prwe_Ao";
 
-
 //Create a new component. This component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar/>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos : [],
+      selectedVideo : null
+    };
+
+    this.videoSearch("Surfboard");
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSearchTermChange={searchKey => this.videoSearch(searchKey)}/>
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} onVideoSelect={selectedVideo => this.setState({selectedVideo})} />
+      </div>
+    );
+  }
+
+  videoSearch(searchKey) {
+   YTSearch({key : API_KEY, term : searchKey}, (videos) => {
+     //setState calls render method again.
+     this.setState({
+       videos : videos,
+       selectedVideo : videos[0]
+     });
+   });
+ }
+
 }
 //Take this component's generated HTML and put it on the page. (in the DOM)
 ReactDOM.render(<App/>, document.querySelector('.container'));
